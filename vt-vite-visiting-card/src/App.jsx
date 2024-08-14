3be03701c5a3a15754200cf5a4f82290f07157dd
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-15 00:27:48 +0530

    feat: Apply css to site

--- vt-vite-visiting-card/src/App.jsx
+++ vt-vite-visiting-card/src/App.jsx
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './App.css'
 
 const defaultTheme = createTheme();
 
