1dc8b8bdfb86f8b5f09df47df21016e225b5badc
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-18 02:09:43 +0530

    feat: Build About component

--- vt-vite-3-route/src/pages/About.jsx
+++ vt-vite-3-route/src/pages/About.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+
 function About() {
+  const [count, setCount] = useState(0);
+  const navigate = useNavigate();
+
+  function handleClick() {
+    // new variable needs to be used since setCount is async
+    const newCount = count + 1;
+    setCount(newCount);
+    if (newCount >= 10) {
+      navigate("/secret");
+    }
+  }
 
   return (
     <>
-      About
+      <h1>About this Website</h1>
+      <p className="subtitle">
+        This website encompasses a secret that you may be able to uncover.
+      </p>
+      <div className='card'>
+        <p>
+          Click this button 10 times to go to Secret Page.
+        </p>
+        <Link to="/">
+          <button>
+            Go to Home
+          </button>
+        </Link>
+        <button onClick={handleClick}>
+          You have clicked {count} times
+        </button>
+      </div>
     </>
   )
 }
