c046b78ed1447605086998a2c776a578d1b62716
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-18 01:57:29 +0530

    feat: Build Contact component

--- vt-vite-3-route/src/pages/Contact.jsx
+++ vt-vite-3-route/src/pages/Contact.jsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom"
+
 function Contact() {
 
   return (
     <>
-      Contact
+      <h1>Contact Page</h1>
+      <p className="subtitle">
+        Come on! This is a simple website. How can you expect a contact us page?
+      </p>
+      <div className='card'>
+        <Link to="/">
+          <button>
+            Go to Home
+          </button>
+        </Link>
+      </div>
     </>
   )
 }
