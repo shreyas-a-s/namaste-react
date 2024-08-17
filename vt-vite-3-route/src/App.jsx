3ef8c943319c3959efe3012ab5f5466f78956022
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-18 01:54:27 +0530

    feat: Build App component

--- vt-vite-3-route/src/App.jsx
+++ vt-vite-3-route/src/App.jsx
@@ -1,10 +1,26 @@
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      App
+      <p className='subtitle'>
+        Welcome to my
+      </p>
+      <h1>Super Secret Website</h1>
+      <div className='card'>
+        <Link to="about">
+          <button>
+            About Us
+          </button>
+        </Link>
+        <Link to="contact">
+          <button>
+            Contact Us
+          </button>
+        </Link>
+      </div>
     </>
   )
 }
