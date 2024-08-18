bfbeb60255bed83c105f3db45a14f7cd6885fa18
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-19 00:47:17 +0530

    feat: Add basic signup functionality

--- vt-vite-login-todo/src/pages/SignUp.jsx
+++ vt-vite-login-todo/src/pages/SignUp.jsx
@@ -1,25 +1,82 @@
 import { Container, TextField, Button, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const SignUp = () => {
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [userList, setUserList] = useState(() => {
+    // Initialize state from localStorage if available
+    const savedUserList = localStorage.getItem('localUserList');
+    return savedUserList ? JSON.parse(savedUserList) : [];
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (userList.some(user => user.email === userData.email)) {
+      alert("Account with the same email id exists!");
+      return
+    }
+
+    if (userData.password !== confirmPassword) {
+      alert("Passwords don't match!");
+      return
+    }
+
+    setUserList([...userList, userData]);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("localUserList", JSON.stringify(userList));
+  }, [userList]);
 
   return (
     <Container maxWidth='sm'>
       <Typography variant='h4' component='h1' gutterBottom>
         Sign Up Page
       </Typography>
-      <TextField label='Name' type='text' fullWidth margin='normal' />
-      <TextField label='Email' fullWidth margin='normal' />
-      <TextField label='Password' type='password' fullWidth margin='normal' />
-      <TextField
-        label='Confirm Password'
-        type='password'
-        fullWidth
-        margin='normal'
-      />
-      <Button variant='contained' color='primary' fullWidth>
-        Sign Up
-      </Button>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label='Name'
+          type='text'
+          value={userData.name}
+          onChange={(e) => setUserData({...userData, name: e.target.value})}
+          fullWidth
+          margin='normal'
+        />
+        <TextField
+          label='Email'
+          type="email"
+          value={userData.email}
+          onChange={(e) => setUserData({...userData, email: e.target.value})}
+          fullWidth
+          margin='normal'
+        />
+        <TextField
+          label='Password'
+          type='password'
+          value={userData.password}
+          onChange={(e) => setUserData({...userData, password: e.target.value})}
+          fullWidth
+          margin='normal'
+        />
+        <TextField
+          label='Confirm Password'
+          type='password'
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          fullWidth
+          margin='normal'
+        />
+        <Button type="submit" variant='contained' color='primary' fullWidth>
+          Sign Up
+        </Button>
+      </form>
       <p style={{ textAlign: 'center' }}>
         Have an account? <Link to='/login'>Log in</Link>
       </p>
