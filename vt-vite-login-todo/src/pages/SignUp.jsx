d2c61771e167d9dbe36a0d2d63e9bf1f14397437
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-18 23:57:06 +0530

    style: Format code

--- vt-vite-login-todo/src/pages/SignUp.jsx
+++ vt-vite-login-todo/src/pages/SignUp.jsx
@@ -1,20 +1,28 @@
-import { Container, TextField, Button, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Container, TextField, Button, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
+
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h4" component="h1" gutterBottom>
+    <Container maxWidth='sm'>
+      <Typography variant='h4' component='h1' gutterBottom>
         Sign Up Page
       </Typography>
-      <TextField label="Name" type="text" fullWidth margin="normal" />
-      <TextField label="Email" fullWidth margin="normal" />
-      <TextField label="Password" type="password" fullWidth margin="normal" />
-      <TextField label="Confirm Password" type="password" fullWidth margin="normal" />
-      <Button variant="contained" color="primary" fullWidth>
+      <TextField label='Name' type='text' fullWidth margin='normal' />
+      <TextField label='Email' fullWidth margin='normal' />
+      <TextField label='Password' type='password' fullWidth margin='normal' />
+      <TextField
+        label='Confirm Password'
+        type='password'
+        fullWidth
+        margin='normal'
+      />
+      <Button variant='contained' color='primary' fullWidth>
         Sign Up
       </Button>
-      <p style={{ textAlign: 'center' }}>Have an account? <Link to="/login">Log in</Link></p>
+      <p style={{ textAlign: 'center' }}>
+        Have an account? <Link to='/login'>Log in</Link>
+      </p>
     </Container>
   )
 }
