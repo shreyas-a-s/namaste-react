431b43152101b425d4104b99f116cf24b44a9866
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-18 23:47:00 +0530

    feat: Apply theme to main.jsx

--- vt-vite-login-todo/src/main.jsx
+++ vt-vite-login-todo/src/main.jsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from '@mui/material/styles';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import theme from './theme';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
