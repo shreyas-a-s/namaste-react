d2e329dafcf8bd6d37bde0ea790703b9346fe685
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-18 23:15:10 +0530

    feat: Create necessary pages and route usinf react-router-dom

--- vt-vite-login-todo/src/App.jsx
+++ vt-vite-login-todo/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Todo from './pages/Todo';
 function App() {
 
   return (
-    <>
-      <p>
-        App
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />}  />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </Router>
   )
 }
 
