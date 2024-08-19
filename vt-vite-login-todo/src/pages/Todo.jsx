e9db0d5e95383ce4d4716a01ba1ca268aa71dc14
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-19 14:03:53 +0530

    feat: Redirect non-logged-in users to Login page

--- vt-vite-login-todo/src/pages/Todo.jsx
+++ vt-vite-login-todo/src/pages/Todo.jsx
@@ -1,6 +1,17 @@
 import { Container, Typography } from '@mui/material';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Todo = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("localUserEmail")) {
+      alert("You must log in to use this app.");
+      navigate("/login");
+    }
+  }, [navigate]);
+
   return (
     <Container>
       <Typography variant="h3" component="h1" gutterBottom>
