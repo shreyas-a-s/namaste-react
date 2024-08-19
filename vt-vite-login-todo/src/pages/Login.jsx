f493eb6774273b0cd0521d360f5ebcd9f2124d61
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-19 13:13:14 +0530

    feat: Add functionality to Login page and redirect to Todo page on successfull login

--- vt-vite-login-todo/src/pages/Login.jsx
+++ vt-vite-login-todo/src/pages/Login.jsx
@@ -1,15 +1,41 @@
 import { Container, TextField, Button, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+  const userList = JSON.parse(localStorage.getItem('localUserList')) || [];
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const user = userList.find(user => user.email === loginData.email);
+
+    if (!user) {
+      alert("Account with this email does not exist!");
+      return;
+    }
+
+    if (user.password !== loginData.password) {
+      alert("Wrong password!");
+      return;
+    }
+
+    localStorage.setItem("localUserData", JSON.stringify(loginData));
+    navigate("/todo");
+  }
+
   return (
     <Container maxWidth="sm">
       <Typography variant="h4" component="h1" gutterBottom>
         Login Page
       </Typography>
-      <form >
-        <TextField label="Email" fullWidth margin="normal" required />
-        <TextField label="Password" type="password" fullWidth margin="normal" required />
+      <form onSubmit={handleSubmit}>
+        <TextField label="Email" value={loginData.email} onChange={(e) => setLoginData({...loginData, email: e.target.value})} fullWidth margin="normal" required />
+        <TextField label="Password" value={loginData.password} onChange={(e) => setLoginData({...loginData, password: e.target.value})} type="password" fullWidth margin="normal" required />
         <Button type="submit" variant="contained" color="primary" fullWidth>
           Login
         </Button>
